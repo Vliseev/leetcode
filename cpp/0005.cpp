0cccecbebccc38254a1fac70e6b4955414ceec7e
Vliseev <Vliseev.92@mail.ru>
2019-05-10 11:20:31 +0300

    0005 in progress

--- cpp/0005.cpp
+++ cpp/0005.cpp
@@ -25,57 +25,122 @@ Output: "bb"
 #include <iostream>
 #include <string>
 #include <vector>
-
+#include <cassert>
+using namespace std;
 
 class Solution {
-public:
-	std::string solution(std::string& s) {
-		using namespace std;
+
+
+	string max_odd(const string &s){
 		int n = s.length();
-		vector<int> d1(n);
-		vector<int> d2(n);
+		vector<int> d(n);
 		int l = 0;
 		int r = -1;
-		int max_1 = 0;
-		int max_2 = 0;
-
+		int max_i = 0;
+		
 		for (int i = 0; i < n; ++i) {
-			int k1 = 0;
-			int k2 = 0;
+			int k = 0;
 
 			if (i <= r) {
-				k1 = min(r - i, d1[r - i + l]);
-				k2 = min(r - i + 1, d2[r - i + l + 1]);
+				k = min(r - i, d[r - i + l]);
 			}
 
-			while (i + k1 + 1 < n && i - k1 - 1 >= 0 &&
-				   s[i + k1 + 1] == s[i - k1 - 1])
-				k1++;
-			d1[i] = k1;
+			while (i + k + 1 < n && i - k - 1 >= 0 &&
+				   s[i + k + 1] == s[i - k - 1])
+				k++;
+			d[i] = k;
 
-			while (i + k2 < n && i - k2 - 1 >= 0 && s[i + k2] == s[i - k2 - 1]) k2++;
-			d2[i] = k2;
-
-			if (i > 0 && d1[max_i] <= d1[i]) max_i = i;
+			if (i > 0 && d[max_i] <= d[i]) max_i = i;
 
 			if (i + k > r) {
 				l = i - k;
 				r = i + k;
 			}
 		}
-		int max_d = d1[max_i];
+		int max_d = d[max_i];
 
-		return s.substr(max_i - max_d, max_i + max_d);
+		return s.substr(max_i - max_d, 2*max_d);
+	}
+	
+	string max_even(const string &s){
+		int n = s.length();
+		vector<int> d(n);
+		int l = 0;
+		int r = -1;
+		int max_i = 0;
+		
+		for (int i = 0; i < n; ++i) {
+			int k = 0;
+
+			if (i <= r) {
+				k = min(r - i, d[r - i + l]);
+			}
+
+			while (i + k < n && i - k - 1 >= 0 &&
+						 s[i + k] == s[i - k - 1])
+			k++;
+			d[i] = k;
+
+			if (i > 0 && d[max_i] <= d[i]) max_i = i;
+
+			if (i + k > r) {
+				l = i - k;
+				r = i + k-1;
+			}
+		}
+		int max_d = d[max_i];
+
+		return s.substr(max_i - max_d, 2*max_d-1);
+	
+	}
+
+
+public:
+	string longestPalindrome(std::string& s) {
+	std::cout<<max_even(s)<<std::endl;
+	std::cout<<max_odd(s)<<std::endl;
+	return "aaa";
 	}
 };
 
+
+string stringToString(string input) {
+    assert(input.length() >= 2);
+    string result;
+    for (int i = 1; i < input.length() -1; i++) {
+        char currentChar = input[i];
+        if (input[i] == '\\') {
+            char nextChar = input[i+1];
+            switch (nextChar) {
+                case '\"': result.push_back('\"'); break;
+                case '/' : result.push_back('/'); break;
+                case '\\': result.push_back('\\'); break;
+                case 'b' : result.push_back('\b'); break;
+                case 'f' : result.push_back('\f'); break;
+                case 'r' : result.push_back('\r'); break;
+                case 'n' : result.push_back('\n'); break;
+                case 't' : result.push_back('\t'); break;
+                default: break;
+            }
+            i++;
+        } else {
+          result.push_back(currentChar);
+        }
+    }
+    return result;
+}
+
 int main() {
-  using namespace std;
-  string s = "babadbaabba";
-  Solution sol;
+    string line;
+		
+	  //string s = "babadbaabba";
+    while (getline(cin, line)) {
+        string s = stringToString(line);
+        
+        string ret = Solution().longestPalindrome(s);
 
-  auto ans = sol.longestPalindrome(s);  // solution(s);
-
-  cout << ans << "\n";
-  return 0;
+        string out = (ret);
+        cout << out << endl;
+    }
+    return 0;
 }
