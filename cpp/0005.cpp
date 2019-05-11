56f8f7974a4029844da57b3aa66eba23b0636ff3
Vliseev <Vliseev.92@mail.ru>
2019-05-11 11:26:43 +0300

    0005 dp is done

--- cpp/0005.cpp
+++ cpp/0005.cpp
@@ -67,3 +67,49 @@ public:
         return even.length() > odd.length() ? even : odd;
     }
 };
+
+
+class Solution_din_prog {
+    bool** alloc_mem(int n) {
+        bool** P = new bool*[n];
+        P[0] = new bool[n * n];
+        for (int i = 1; i < n; ++i) P[i] = P[0] + i * n;
+        std::fill(P[0], P[0]+n*n, false);
+        return P;
+    }
+
+    void dealloc_mem(bool** P) {
+        delete[] P[0];
+        delete[] P;
+    }
+
+public:
+    string longestPalindrome(std::string& s) {
+        int n = s.size();
+        if(n==0)
+            return "";
+        bool** P = alloc_mem(n);
+        int max_i = 0, max_j = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            P[i][i + 1] = s[i] == s[i + 1];
+            if (P[i][i + 1]) max_j = i, max_i = 1;
+            P[i][i] = true;
+        }
+        P[n - 1][n - 1] = true;
+
+        for (int i = 2; i < n; ++i) {
+            for (int j = 0; j + i < n; j++) {
+                if (P[j + 1][j + i - 1] && (s[j] == s[j + i])) {
+                    P[j][j + i] = true;
+                    if (i > max_i) {
+                        max_j = j;
+                        max_i = i;
+                    }
+                }
+            }
+        }
+        dealloc_mem(P);
+        return s.substr(max_j, max_i + 1);
+    }
+};
